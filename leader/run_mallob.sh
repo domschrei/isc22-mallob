f4917ad8ce8b3af174eda6755e265b17e9e145be
Dominik Schreiber <mail@dominikschreiber.de>
2022-04-27 09:46:30 +0200

    Add proper options

--- leader/run_mallob.sh
+++ leader/run_mallob.sh
@@ -1,17 +1,23 @@
 #!/bin/bash
 
+processes_per_host=$(cat $1|grep -oE "slots=[0-9]+"|head -1|grep -oE "[0-9]+")
+
 command="mpirun --mca btl_tcp_if_include eth0 --allow-run-as-root --hostfile $1 \
---use-hwthread-cpus --map-by node:PE=2 --bind-to none --report-bindings \
+--use-hwthread-cpus --map-by node:PE=$processes_per_host --bind-to none --report-bindings \
 `# Executable & formula input` \
 mallob -mono=$2 \
 `# General options` \
--sleep=1000 -t=4 -appmode=fork -nolog -v=3 -interface-fs=0 -trace-dir=competition -subproc-dir=. \
+-sleep=1000 -t=4 -appmode=fork -nolog -v=3 -interface-fs=0 -trace-dir=competition -subproc-dir=. -max-lits-per-thread=50000000 -pipe-large-solutions=0 -processes-per-host=$processes_per_host -regular-process-allocation=1 -strict-clause-length-limit=20 -clause-buffer-discount=0.9 \
 `# SAT solving options` \
--satsolver=kclg"
+-clause-filter-clear-interval=300 -max-lbd-partition-size=2 -export-chunks=20 \
+`# Kicaliglu portfolio` \
+-satsolver=kkclkkclkkclkkclccgg `# 8 Kissat, 6 CaDiCaL, 4 Lingeling, 2 Glucose`
+`# Ki, Kiki portfolio` \
+`#-satsolver=k`"
 
 echo "run_mallob.sh : EXECUTE $command"
 
-# Workaround for MPI error messages "Read -1, expected <some number>, errno = 1"
+# Workaround for MPI error messages: Read -1, expected <some number>, errno = 1
 OMPI_MCA_btl_vader_single_copy_mechanism=none
 export OMPI_MCA_btl_vader_single_copy_mechanism=none
 
